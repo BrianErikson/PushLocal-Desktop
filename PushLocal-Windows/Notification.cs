3682cf077e2703eb62e2be0bdb2f26594c099270
Brian Erikson <briankerikson@gmail.com>
2015-08-24 21:55:26 +0200

    Windows - working on installing shortcut for appID

--- PushLocal-Windows/Notification.cs
+++ PushLocal-Windows/Notification.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using Windows.Data.Xml.Dom;
 using Windows.Data.Xml;
 using Windows.UI.Notifications;
+using IWshRuntimeLibrary;
 
 namespace PushLocalWindows
 {
 	public class Notification
 	{
+		public static string APP_ID = "Beariksonstudios.PushLocal";
+		public static string EXE_PATH = Path.GetDirectoryName (System.AppDomain.CurrentDomain.BaseDirectory) + "PushLocal.exe";
+		public static string APP_START_MENU_PATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu), "Programs", "PushLocal");
+		public static string SHORTCUT_LOCATION = Path.Combine(APP_START_MENU_PATH, "PushLocal" + ".lnk");
+
 		public static void Post(string header, string text, string subtext) {
+			if (!HasShortcut ())
+				CreateShortcut ();
+
 			// Get a toast XML template
 			XmlDocument toastXml = (XmlDocument) ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
-
 
 			// Fill in the text elements
 			XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
@@ -26,10 +35,26 @@ namespace PushLocalWindows
 			ToastNotification toast = new ToastNotification(toastXml);
 			// TODO toast.Activated listener
 			ToastNotifier notifier = ToastNotificationManager.CreateToastNotifier();
-			if (notifier == null)
-				Console.WriteLine ("Notifier null");
 
 			notifier.Show (toast);
+		}
+
+		public static void CreateShortcut() {
+			// TODO Access to the path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PushLocal' is denied.
+			//if (!Directory.Exists(APP_START_MENU_PATH))
+			//	Directory.CreateDirectory(APP_START_MENU_PATH);
+
+			WshShell shell = new WshShell();
+			shell.RegWrite ("{9F4C2855-9F79-4B39-A8D0-E1D42DE1D5F3}, 5", APP_ID, (ushort)VarEnum.VT_LPWSTR);
+            IWshShortcut shortcut = shell.CreateShortcut(SHORTCUT_LOCATION) as IWshShortcut;
+			shortcut.Description = "PushLocal Desktop App";
+			//shortcut.IconLocation = @"C:\Program Files (x86)\TestApp\TestApp.ico"; //uncomment to set the icon of the shortcut
+			shortcut.TargetPath = EXE_PATH;
+			shortcut.Save(); 
+		}
+
+		public static bool HasShortcut() {
+			return System.IO.File.Exists (SHORTCUT_LOCATION);
 		}
 	}
 }
